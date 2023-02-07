5ddbe8b4388a24d161ec791e51c76e83e1aca592
Anca Puscasu <ancapuscasu@ymail.com>
2023-02-07 13:53:30 -0500

    Reverting to 9097643

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,16 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/ancapuscasu/Documents/GitHub/phase-2/src/app/page": [
+      "/Users/ancapuscasu/Documents/GitHub/phase-2/src/app/page.module.css",
+      "/Users/ancapuscasu/Documents/GitHub/phase-2/node_modules/@next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/ancapuscasu/Documents/GitHub/phase-2/src/app/globals.css"
+    ]
+  },
+  "/Users/ancapuscasu/Documents/GitHub/phase-2/src/app/layout.tsx": [
+    "/Users/ancapuscasu/Documents/GitHub/phase-2/src/app/globals.css"
+  ],
+  "/Users/ancapuscasu/Documents/GitHub/phase-2/src/app/page.tsx": [
+    "/Users/ancapuscasu/Documents/GitHub/phase-2/src/app/page.module.css",
+    "/Users/ancapuscasu/Documents/GitHub/phase-2/node_modules/@next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+  ]
+}
