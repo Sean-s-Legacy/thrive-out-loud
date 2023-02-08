1004758b781e0da53a41ed9459a0daf75778ca4b
Oluwadunsin Ogunleye <aloroduns@yahoo.com>
2023-02-08 06:08:44 -0500

    Installed Next.js v-13 using npm

--- .next/server/font-loader-manifest.js
+++ .next/server/font-loader-manifest.js
@@ -1,8 +1,4 @@
 self.__FONT_LOADER_MANIFEST={
   "pages": {},
-  "app": {
-    "/Users/ancapuscasu/Documents/GitHub/phase-2/node_modules/@next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
-      "static/media/2aaf0723e720e8b9.p.woff2"
-    ]
-  }
+  "app": {}
 }
