4b7cb5193101252530e3057893923961df7640ce
Anca Puscasu <ancapuscasu@ymail.com>
2023-02-13 18:26:09 -0500

    Started setting up Firebase and creating user schema

---
+++ utils/db/index.js
@@ -0,0 +1,15 @@
+import admin from "firebase-admin";
+
+import serviceAccount from "./service-account-key.json";
+
+if (!admin.apps.length) {
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+    });
+  } catch (error) {
+    console.log("Firebase admin initialization error", error.stack);
+  }
+}
+
+export default admin.firestore();
