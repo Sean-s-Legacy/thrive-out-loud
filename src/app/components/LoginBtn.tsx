9aa732f20d1821c125d8f36ad3cac89197c9e3be
aya222222 <80786703+aya222222@users.noreply.github.com>
2023-02-17 20:09:39 -0500

    Landing page.

---
+++ src/app/components/LoginBtn.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'antd'
+import React from 'react'
+
+export default function LoginBtn() {
+  return (
+    <Button type="text" className='common_btn auth_btn'>
+       Login
+    </Button>
+  )
+}
