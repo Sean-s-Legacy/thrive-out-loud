04ca0a75adf6793e95b0cb6172814effb064e349
rusevaIvelina <iva.ruseva14@gmail.com>
2023-02-27 21:48:39 -0500

    added email and website

--- src/components/FooterNotAuth/index.tsx
+++ src/components/FooterNotAuth/index.tsx
@@ -20,9 +20,23 @@ export default function FooterNotAuth() {
                <Button>Sign up</Button>
                </div>
                <Divider className={styles.divider}/>
+
+                <div className={styles.container2}>
                <div className="col-12 col-md-3">
-                
+               <Image alt="treelogo" width={20} height={16} src="/images/Icon.png"/>
+               <h4 className={styles.emailtext}>Email</h4>
+               <h4 className={styles.text}>Our friendly team is here to help.</h4>
+               <h4 className={styles.text}>seanslegacyplaceholder@gmail.com</h4>
                </div>
+             
+               <div className="col-12 col-md-3">
+               <Image alt="treelogo" width={20} height={16} src="/images/Iconarrow.png"/>
+               <h4 className={styles.emailtext}>Website</h4>
+               <h4 className={styles.text}>Come checkout our other programs!</h4>
+               <h4 className={styles.text}>seanslegacy.org</h4>
+              </div>
+              </div>
+
             </div>
           </div>
         </div>
