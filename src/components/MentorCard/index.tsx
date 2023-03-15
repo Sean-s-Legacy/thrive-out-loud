ac0ceb892eb04fee10fbb9c2879a9d73461559d8
aya222222 <80786703+aya222222@users.noreply.github.com>
2023-03-14 20:42:31 -0400

    mentor card

--- src/components/MentorCard/index.tsx
+++ src/components/MentorCard/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
+
 import { Divider, Typography, Button, Space, Card } from "antd";
+import styles from "./MentorCard.module.css";
+
 const { Title, Paragraph, Text, Link } = Typography;
 
 export default function MentorCard({
@@ -13,16 +16,38 @@ export default function MentorCard({
 }) {
   return (
     <Card
-      style={{ width: 348, height: 520 }}
+      style={{
+        width: 348,
+        height: 520,
+        border: "none",
+        backgroundColor: "#f9f9f9",
+      }}
+      bordered={false} /*it seems it's not working */
       hoverable
-      cover={<img alt="example" src={imgSrc} />}
+      cover={
+        <img
+          alt="example"
+          src={imgSrc}
+          style={{ height: 340, objectFit: "cover" }}
+        />
+      }
     >
-      <p>
-        {name} {pronouns}
-      </p>
-      <p>{jobTitle}</p>
-      <p>{fieldOfExpertise}</p>
-      <p>{supportOffered}</p>
+      <div>
+        <h5>
+          {name} {pronouns}
+        </h5>
+        <h5>{jobTitle}</h5>
+        <br />
+        {fieldOfExpertise.map((el: string, i: number) => {
+          return <span key={i}>{el} </span>;
+        })}
+        <br />
+        <br />
+        {supportOffered.map((el: string, i: number) => {
+          return <span key={i}> {el}</span>;
+        })}
+        <br />
+      </div>
     </Card>
   );
 }
