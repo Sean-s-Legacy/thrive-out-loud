5d3e9b715839d83991220fdf39a204aa89877505
rusevaIvelina <iva.ruseva14@gmail.com>
2023-03-16 00:02:30 -0400

    env/firebase

--- src/Firebase/config.ts
+++ src/Firebase/config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY || " ",
   authDomain: `${process.env.NEXT_PUBLIC_PROJECT_ID}.firebaseapp.com`,
   databaseURL: `https://${process.env.NEXT_PUBLIC_PROJECT_ID}.firebaseio.com`,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: `${process.env.NEXT_PUBLIC_PROJECT_ID}.appspot.com`,
+  storageBucket:`${process.env.NEXT_PUBLIC_PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_API_ID,
-  measurementId: `G-${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`,
+  measurementId: `G-${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`
 };
