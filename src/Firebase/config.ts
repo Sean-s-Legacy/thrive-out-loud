65653124a597e0ca4e914b8b5af101d7e4e5d69c
Anca Puscasu <ancapuscasu@ymail.com>
2023-03-15 20:34:45 -0400

    Update config.ts

--- src/Firebase/config.ts
+++ src/Firebase/config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  apiKey: process.env.NEXT_APP_API_KEY,
-  authDomain: `${process.env.NEXT_APP_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: `https://${process.env.NEXT_APP_PROJECT_ID}.firebaseio.com`,
-  projectId: process.env.NEXT_APP_PROJECT_ID,
-  storageBucket: `${process.env.NEXT_APP_PROJECT_ID}.appspot.com`,
-  messagingSenderId: process.env.NEXT_APP_SENDER_ID,
-  appId: process.env.NEXT_APP_API_ID,
-  measurementId: `G-${process.env.NEXT_APP_MEASUREMENT_ID}`,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: `${process.env.NEXT_PUBLIC_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.NEXT_PUBLIC_PROJECT_ID}.firebaseio.com`,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: `${process.env.NEXT_PUBLIC_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_API_ID,
+  measurementId: `G-${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`,
 };
