a38f967164fb4e6311005696c039bd8bde976f36
Oluwadunsin Ogunleye <aloroduns@yahoo.com>
2023-03-18 11:56:55 -0400

    list all user controller endpoints

---
+++ .history/functions/src/routes_20230314162502.ts
@@ -0,0 +1,15 @@
+import {Application}  from "express"
+import userRouter from "./user/routes"
+
+export const routesConfig = ( app: Application) => {
+    app.use("/users", userRouter)
+}
+
+// app.ts
+import { getAllUsers } from './user';
+
+const app = express();
+
+app.get('/users', getAllUsers);
+
+app.listen(3000, () => console.log('Server started'));
---
+++ .history/functions/src/routes_20230314170720.ts
@@ -0,0 +1,9 @@
+import {Application}  from "express"
+import userRouter from "./user/routes"
+
+export const routesConfig = ( app: Application) => {
+    app.use("/users", userRouter)
+}
+
+// app.ts
+
