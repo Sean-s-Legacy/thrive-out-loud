13d606d230076e285bc9579e2e3e511ad3061d94
Anca Puscasu <ancapuscasu@ymail.com>
2023-03-27 00:27:35 -0400

    Update AppLayout.tsx

--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
 
-import { useAuth } from "../../context/AuthContext";
+import { useAuth } from "@/context/AuthContext";
 
 export const AppLayout = ({ children }) => {
   const { currentUser } = useAuth();
