de62835de0025b26cafcb5a5648d225c054402fa
rusevaIvelina <iva.ruseva14@gmail.com>
2023-03-27 20:13:51 -0400

    created a section from the new landing page

---
+++ src/pages/index2.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Typography, Row, Col, Button } from "antd";
+import Image from "next/image";
+import styles from "@/styles/index2.module.css";
+
+
+export default function newhomepage() {
+  const { Title, Text } = Typography;
+  return (
+    <>
+    <div>
+    <section className={styles.transitionSection}>
+              <div className={styles.transitionSectionContainer}>
+                <Text className="mediumWeight textLarge textInverse">
+                  At Sean’s Legacy, we’re dedicated to providing support for
+                  LGBTQIA+ youth in their academic and extracurricular
+                  endeavors. Whether you’re thinking about going to college,
+                  looking for your first job, or just need some support and
+                  guidance, our online community of LGBTQIA+ mentors is here to
+                  help.
+                </Text>
+              </div>
+            </section>
+
+            <section>
+              <div className={styles.space}>
+                <Title level={2} className='bold textAlignCenter textPrimary7'>Why sign up as a mentee?</Title>
+              </div>
+    
+              <div className={styles.container1}>
+              <Row align='middle'>
+              <Col span ={8}>
+                <div>
+                <Image
+                alt="100% free promotional text "
+                width={368}
+                height={320}
+                src="/images/Frame 51336piggybank.png"
+               />
+              </div>
+              
+              </Col>
+
+               <Col span={8}>
+              <div className='space-12'>
+              <Image
+                alt="focused on you promotional text "
+                width={368}
+                height={320}
+                src="/images/Frame 51337crown.png"
+               />
+              </div>
+              </Col>
+
+              <Col span={8}>
+              <div >
+              <Image
+                alt="designed for privacy promotional text "
+                width={368}
+                height={320}
+                src="/images/Frame 51338lock.png"
+               />
+              </div>
+              </Col>
+
+              </Row>
+              </div>
+              <div className={styles.learnbtn}>
+                <Button className={styles.learnbtn} size='large' type='primary'>Learn more </Button>
+              </div>
+             </section>
+              </div>
+            
+    </>
+  )
+}
