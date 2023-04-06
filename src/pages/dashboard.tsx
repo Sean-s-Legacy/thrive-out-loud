5f267f375960818a475cdef0c7573372eb94f4d8
Anca Puscasu <ancapuscasu@ymail.com>
2023-04-06 14:27:55 -0400

    testing protected route

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,14 +1,30 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 import Mentors from "../components/Mentors";
 
 import { useAuth } from "../context/AuthContext";
 
 export default function Dashboard() {
   const { currentUser } = useAuth();
+  const router = useRouter();
+
   console.log("dashboard currentUser:", currentUser);
+
+  useEffect(() => {
+    if (currentUser == null) {
+      router.push("/");
+    }
+  }, [currentUser, router]);
+
+  // if (!currentUser) {
+  //   // user is signed out or still being checked.
+  //   // don't render anything
+  //   return null;
+  // }
 
   return (
     <div>
-      Welcome <b>{currentUser?.displayName} this is the dashbaord</b>
+      Welcome, <b>{`${currentUser?.displayName}!`}</b> this is the dashboard
       <div></div>
       <Mentors />
     </div>
