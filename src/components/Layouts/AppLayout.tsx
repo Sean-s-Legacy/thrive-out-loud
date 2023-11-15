02415d882304a41d61981795284793a57be6562d
Jane Cho <janechodance@gmail.com>
2023-11-15 14:55:03 -0500

    minimum info page

--- src/components/Layouts/AppLayout.tsx
+++ src/components/Layouts/AppLayout.tsx
@@ -2,17 +2,31 @@
 import React from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import NavbarOnlyLogo from "./NavbarOnlyLogo";
 
 import { useAuth } from "@/context/AuthContext";
+import { usePathname } from "next/navigation";
 
 export const AppLayout = ({ children }) => {
   const { currentUser } = useAuth();
-
+  const route = usePathname();
+  const conditions = [
+    "minimumInfo",
+    "emailVerification",
+    "emailVerificationSucess",
+    "numberVerification",
+  ];
   return (
     <>
-      <Navbar user={currentUser} />
+      {conditions.some((str) => route.includes(str)) ? (
+        <NavbarOnlyLogo />
+      ) : (
+        <Navbar user={currentUser} />
+      )}
       {children}
-      <Footer user={currentUser} />
+      {conditions.some((str) => route.includes(str)) ? null : (
+        <Footer user={currentUser} />
+      )}
     </>
   );
 };
