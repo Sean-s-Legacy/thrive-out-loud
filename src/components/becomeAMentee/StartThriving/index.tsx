d9bd32cf95140acf1700a26bd19dc67478ee4063
oxangyal <Oxana.mitch@gmail.com>
2024-04-08 13:20:51 -0400

    StartThriving component

---
+++ src/components/becomeAMentee/StartThriving/index.tsx
@@ -0,0 +1,36 @@
+import { Button, Link, Typography } from "antd";
+
+import Image from "next/image";
+import React from "react";
+import styles from "./StartThriving.module.css";
+
+function Index() {
+    const { Title, Text } = Typography;
+    return (
+        <>
+            <section className={styles.section}>
+                <div className={styles.container}>
+                    <div className={styles.title}>
+                        <Title level={1}
+                            className="textPrimary8 bold">
+                            Start Thriving
+                        </Title>
+                    </div>
+                    <div className={styles.button}>
+                        <Button type="primary" className="textPrimary8">
+                            Sign Up
+                        </Button>
+                    </div>
+                    <div className={styles.typography}>
+                        <Text>
+                            Thrive Out Loud is part of a network of community
+                            initiatives led by Sean’s Legacy.
+                        </Text>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default Index;
