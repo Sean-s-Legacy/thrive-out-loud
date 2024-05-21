444b3915c136c5f0cbaffa873bb9ac40a50359fe
Mami. N <mamypoco@gmail.com>
2024-05-21 11:43:58 -0700

    updated mentee and mentor's onboarding welcome screen

--- src/components/Onboarding/Mentor/Welcome/index.tsx
+++ src/components/Onboarding/Mentor/Welcome/index.tsx
@@ -1,33 +1,53 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Button } from "antd";
+import { Typography, Button, Input, Checkbox } from "antd";
 import Link from "next/link";
+import styles from "./Welcome.module.css";
+import Image from "next/image";
 
-export default function Welcome(){
+export default function Welcome() {
+  const { Title, Paragraph } = Typography;
   return (
-    <div>
-    <h1>Welcome to Thrive Out Loud</h1>
-    <div>
-      We’re so excited you’re here!
+    <div className={styles.welcomeContainer}>
+      <div className={styles.contentWrapper}>
+        <div className={styles.titleWrapper}>
+          <Title className="bold">Welcome to Thrive Out Loud&nbsp;</Title>
+          <Image
+            alt="shining star"
+            src={"images/onboarding/star-icon.svg"}
+            width={36}
+            height={36}
+          />
+        </div>
+        <Paragraph>We&apos;re so excited you&apos;re here!</Paragraph>
+        <Paragraph className="bold">
+          Just a heads up: Our platform is currently open to mentors who live in
+          the United States and are 18 years or older.
+        </Paragraph>
+        <Paragraph>
+          If that&apos;s not you, we hope to expand our program in the future,
+          so please check back later.
+        </Paragraph>
+        <Paragraph>
+          Thank you for joining our community!&nbsp;
+          <span className="bold">
+            We&apos;ll begin by asking you a few questions about your identify
+            and background&nbsp;
+          </span>
+          to personalize your mentorship experience.
+        </Paragraph>
+        <Checkbox className={styles.checkboxWrapper}>
+          I am at least 18 years or older
+        </Checkbox>
+
+        <div className={styles.twoButtons}>
+          <Link href="/">
+            <Button>Home</Button>
+          </Link>
+          <Link href="/menteeQuestionnaire">
+            <Button type="primary">Let&apos;s Go</Button>
+          </Link>
+        </div>
+      </div>
     </div>
-    <div>
-      <span className="semibold">Please note: Our platform currently supports mentees who live in the United States and are 18 years or older.</span>
-      Unfortunately, we are unable to consider mentees from outside of the U.S. at this time.
-    </div>
-    <div>
-      Thank you for joining our community!
-      <span> Let’s kickstart your mentorship journey by getting to know you </span>
-      so we can match you with the right mentors.
-    </div>
-    <Link href="/">
-      <Button>
-        Home
-      </Button>
-    </Link>
-    <Link href="/mentorQuestionnaire">
-      <Button type="primary">
-        Let’s Go
-      </Button>
-    </Link>
-  </div>
-  )
+  );
 }
